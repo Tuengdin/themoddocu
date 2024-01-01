46c53c3e7815dcbefffee3500670c097c235c6ea
tdin <tuengdin@gmail.com>
2024-01-01 16:14:52 +0700

    add build

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>TheModDocu</span>,
+  logo: <span>document.ModTdin</span>,
   project: {
     link: "https://github.com/Tuengdin/themoddocu",
   },
 
   docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
-    text: "TheModDocu",
+    text: "document.ModTdin.com",
   },
 };
 
